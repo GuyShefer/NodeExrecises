f7d06d3f289be76477297b807d7129a1daf60bd3
Guy <shefer.guy1@gmail.com>
2021-04-11 16:10:39 +0300

    .

---
+++ 2.1 - file system/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'File was created by me')
+
+// fs.appendFileSync('notes.txt', ' data to append');
+
+// fs.copyFile('notes.txt','destination.txt', (err)=> {
+//     if(err) throw err
+// })
+
+// fs.renameSync('newFile.txt', 'newFile2.txt');
+
+// const files = fs.readdirSync('./');
+// files.forEach(file => console.log(file));
+
+// const words = fs.mkdirSync('notes');
