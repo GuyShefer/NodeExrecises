c7d2504085ac985d9f59b5f510e4d89c3e7fda99
Guy <shefer.guy1@gmail.com>
2021-04-11 14:07:26 +0300

    first commit

---
+++ 1.  - node vs js.js
@@ -0,0 +1,12 @@
+// 3 things node.js has that the client side javascript doesn’t
+// have: 
+
+// 1. non-blocking operation
+// 2. global object
+
+// 3 things the client side javascript has that node.js doesn’t
+// have
+
+// 1. window object - is something specifeally provided by the chrome runtime when js is running in the chrome app.
+// 2. local storage
+// 3. document
