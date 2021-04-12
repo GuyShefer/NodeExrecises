6a3f9148ea884c51bf351115220220c503764828
Guy <shefer.guy1@gmail.com>
2021-04-12 13:37:54 +0300

    .

---
+++ practice/index.js
@@ -0,0 +1,41 @@
+const { equal } = require('assert');
+const http = require('http');
+const port = 8001;
+const url = require('url')
+
+const users = [
+    { id: 0, name: 'Michael', capsuleNumber: 1 },
+    { id: 1, name: 'Gilad', capsuleNumber: 1 },
+    { id: 2, name: 'Taly', capsuleNumber: 1 },
+    { id: 3, name: 'Shir', capsuleNumber: 1 },
+    { id: 4, name: 'Osama', capsuleNumber: 1 },
+    { id: 5, name: 'Guy', capsuleNumber: 1 },
+    { id: 6, name: 'Shlomi', capsuleNumber: 2 },
+    { id: 7, name: 'Or', capsuleNumber: 2 },
+    { id: 8, name: 'Rachel', capsuleNumber: 2 },
+]
+
+const server = http.createServer((req, res) => {
+    const query = url.parse(req.url, true).query;
+    console.log(query.id);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    if (req.method === 'GET') {
+        if (req.url === '/getAll') {
+            res.write('HEY');
+        }
+        else if (req.url === '/getUsers') {
+            res.write(JSON.stringify(users));
+        }
+        else if (req.url.includes('user')) {
+            res.write(JSON.stringify(users[query.id]));
+        }
+        else if(req.url.includes('byCapsule')){
+            res.write(JSON.stringify(users.filter(user => user.capsuleNumber == query.id)));
+        }
+    }
+    res.end();
+})
+
+server.listen(port, () => {
+    console.log('server run on port ' + port);
+})
