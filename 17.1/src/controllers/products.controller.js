ac3c952bbab65bc1f974f3da267ca4ee76870ec6
Guy <shefer.guy1@gmail.com>
2021-04-20 22:55:05 +0300

    .

--- 17.1/src/controllers/products.controller.js
+++ 17.1/src/controllers/products.controller.js
@@ -15,6 +15,54 @@ const addProduct = (req, res) => {
 
 }
 
+const getAllUsers = (req, res) => {
+    Product.find({}).then((products) => {
+        res.status(200).send(products);
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+}
+
+const getProductByName = (req,res) => {
+    const productName = req.params.name;
+    Product.findOne({name: productName}).then(product => {
+        if(!product) {
+            return res.status(404).send();
+        }
+        res.send(product);
+    }).catch(err => {
+        res.status(501).send(err);
+    })
+}
+
+const getAllActiveProducts = (req,res) => {
+    Product.find({isActive: true}).then((products) => {
+        res.status(200).send(products);
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+}
+
+const getAllProductsInPriceRange = (req,res) => {
+    const minPrice = req.params.min;
+    const maxPrice = req.params.max;
+    // Product.find({}).where('details.price').gt(minPrice).lt(maxPrice).exec( x => {
+    //     console.log(x);
+    // })
+    Product.find({'details.price': {$gte : minPrice, $lte : maxPrice }}).then(products => {
+        if(!products) {
+            return res.status(404).send();
+        }
+        res.send(products);
+    }).catch(err => {
+        res.status(501).send(err);
+    })
+}
+
 module.exports = {
     addProduct,
+    getAllUsers,
+    getProductByName,
+    getAllActiveProducts,
+    getAllProductsInPriceRange,
 }
--- 17.1/src/routes/products.routes.js
+++ 17.1/src/routes/products.routes.js
@@ -6,4 +6,19 @@ router.post('/', (req, res) => {
     productController.addProduct(req, res);
 })
 
+router.get('/', (req, res) => {
+    productController.getAllUsers(req, res);
+})
+
+router.get('/byName/:name', (req, res) => {
+    productController.getProductByName(req, res);
+})
+
+router.get('/byActivity', (req, res) => {
+    productController.getAllActiveProducts(req, res);
+})
+
+router.get('/byPriceRange/:min/:max', (req, res) => {
+    productController.getAllProductsInPriceRange(req, res);
+})
 module.exports = router;
