c9df0616a31f89b99fe379852fb5dd13c0d5f8b4
Guy <shefer.guy1@gmail.com>
2021-04-21 12:11:15 +0300

    .

--- 17.1/src/controllers/products.controller.js
+++ 17.1/src/controllers/products.controller.js
@@ -1,59 +1,66 @@
 const Product = require('../models/product.model');
 
-const addProduct = (req, res) => {
+const addProduct = async (req, res) => {
     const extractProduct = {} = req.body;
-    console.log(req.body);
+
     // validations // 
 
-    const product = new Product(extractProduct);
-
-    product.save().then(() => {
+    try {
+        const product = new Product(extractProduct);
+        product.save();
         res.status(201).send(product);
-    }).catch((err) => {
+    } catch (err) {
         res.status(400).send(err);
-    });
-
+    }
 }
 
-const getAllUsers = (req, res) => {
-    Product.find({}).then((products) => {
+const getAllUsers = async (req, res) => {
+
+    try {
+        const products = await Product.find({});
         res.status(200).send(products);
-    }).catch((err) => {
+    } catch (err) {
         res.status(400).send(err);
-    })
+    }
 }
 
-const getProductByName = (req,res) => {
+const getProductByName = async (req, res) => {
     const productName = req.params.name;
-    Product.findOne({name: productName}).then(product => {
-        if(!product) {
+
+    try {
+        const product = await Product.findOne({ name: productName });
+        if (!product) {
             return res.status(404).send();
         }
         res.send(product);
-    }).catch(err => {
+    } catch (err) {
         res.status(501).send(err);
-    })
+    }
 }
 
-const getAllActiveProducts = (req,res) => {
-    Product.find({isActive: true}).then((products) => {
+const getAllActiveProducts = async (req, res) => {
+
+    try {
+        const products = await Product.find({ isActive: true });
         res.status(200).send(products);
-    }).catch((err) => {
+    } catch (err) {
         res.status(400).send(err);
-    })
+    }
 }
 
-const getAllProductsInPriceRange = (req,res) => {
+const getAllProductsInPriceRange = async (req, res) => {
     const minPrice = req.params.min;
     const maxPrice = req.params.max;
-    Product.find({'details.price': {$gte : minPrice, $lte : maxPrice }}).then(products => {
-        if(!products) {
+
+    try {
+        const products = await Product.find({ 'details.price': { $gte: minPrice, $lte: maxPrice } });
+        if (!products) {
             return res.status(404).send();
         }
         res.send(products);
-    }).catch(err => {
+    } catch (err) {
         res.status(501).send(err);
-    })
+    }
 }
 
 module.exports = {
