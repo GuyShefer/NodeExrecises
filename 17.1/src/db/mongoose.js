12ef1eae97ab8574f0e25b9efbcda950d803548d
Guy <shefer.guy1@gmail.com>
2021-04-21 09:00:04 +0300

    .

--- 17.1/src/db/mongoose.js
+++ 17.1/src/db/mongoose.js
@@ -7,23 +7,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/e-commerce-site', {
 }).then (() => {
     console.log('connected succsessful');
 })
-
-
-// const product1 = new Product({
-//     name: 'Iphone13',
-//     category : 'Electricity',
-//     isActive: true,
-//     details : {
-//         description: "asdasd asdasd asdasd",
-//         price: 50,
-//         discount: 10,
-//         images: ['asdasdasd', 'asdasdasd'],
-//         phone: '0501234567',  
-//     }
-// })
-
-// product1.save().then(() => {
-//     console.log(product1);
-// }).catch((err) => {
-//     console.log('Error', err);
-// })
